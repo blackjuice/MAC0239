3f52feda40792ff03e3c60c2e35355cc9200cf84
blackjuice <sagua.holic@gmail.com>
2015-12-13 00:10:37 -0500

    Get highest number on lista rotulos

--- EP2/ep2_8516816_8124329.py
+++ EP2/ep2_8516816_8124329.py
@@ -4,15 +4,11 @@
     Lucas Sung Jun Hong  (8124329)
 
 '''
-
-#from parser import CTLtree
-#CTLtree = CTLtree()
-#return CTLtree.run()
 from parser import CTLtree
 #from subprocess import call
 from pyeda.inter import *
 import os
-
+import re
 #------------main--------------
 '''
 '''
@@ -22,28 +18,32 @@ rotulos = input()
 formulaCTL = str( CTLtree( input() ) ) #formulaCTL = input(); formulaCTL = str( CTLtree(formulaCTL) )
 interest = input()
 
-numeroEstados = 10
-# Tipo 1
-dt = {0: ()} # inicializando dicionario temporario
-for i in range( int(numeroEstados) ):
-    dictionary = {i: ''}
-    print (i, dictionary)
-    dt.update( dictionary )
-    dictionary.update( dt )
-print (dictionary)
+'''
+Tipo 1: Sem regex (problema quando i > 9)
+-----------------------------------------
+    real    0m0.892s
+    user    0m0.560s
+    sys     0m0.056s
+print (rotulos)
+'''
+#rotulos = "[(“x1”,”x2”),(“x2”,“x3”),(“x100”)]"
+m1 = " ".join(rotulos)
+#m1 = m1 * 1000
+a = [int(s) for s in m1.split() if s.isdigit()]
+print (a)
+print(max(a))
 
-# Tipo 2
-lst = [i for i in range( int(numeroEstados) )] * 1
-print (lst)
-ddt = {el:0 for el in lst}
-print (ddt)
+'''
+Tipo 2: Com regex
+-----------------------------------------
+    real    0m0.413s
+    user    0m0.384s
+    sys     0m0.028s
 
-# Tipo 3
-lst3 = [i for i in range( int(numeroEstados) )] * 1
-ddt.fromkeys(lst3)
-print (ddt)
-
-# Tipo 4
-lst4 = [0 for i in range( int(numeroEstados) )]
-ddt.fromkeys(lst3)
-print (ddt)
+'''
+#rotulos = "[(“x1”,”x2”),(“x2”,“x3”),(“x100”)]"
+m2 = re.sub(r'\D', " ", rotulos)
+m2 = m2 * 10000
+a = [int(s) for s in m2.split() if s.isdigit()]
+print (a)
+print(max(a))

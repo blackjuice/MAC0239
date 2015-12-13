9a1cbbddade9444fb46eef6b9981b2106adba284
leodaneu <leonardodaneu93@gmail.com>
2015-12-13 18:23:36 -0200

    Update functions.py

--- EP2/functions.py
+++ EP2/functions.py
@@ -2,16 +2,16 @@
     # Bf = Bx', Bg = B->
     def Apply(op, Bf, Bg):
         str = Bf + str(op) + Bg
-        if (Bf.lo ~= None and Bf.hi ~= None) and (Bg.lo ~= None and Bg.hi ~= None)
+        if (Bf.lo ~= None and Bf.hi ~= None) and (Bg.lo ~= None and Bg.hi ~= None):
             Apply(op, Bf.hi, Bg.hi)
             Apply(op, Bf.lo, Bg.lo)
-        if (Bf.lo ~= None and Bf.hi ~= None) and (Bg.lo == None and Bg.hi == None)
+        if (Bf.lo ~= None and Bf.hi ~= None) and (Bg.lo == None and Bg.hi == None):
             Apply(op, Bf.hi, Bg)
             Apply(op, Bf.lo, Bg)
-        if (Bf.lo == None and Bf.hi == None) and (Bg.lo ~= None and Bg.hi ~= None)
+        if (Bf.lo == None and Bf.hi == None) and (Bg.lo ~= None and Bg.hi ~= None):
             Apply(op, Bf, Bg.hi)
             Apply(op, Bf, Bg.lo)
-        if (Bf.lo == None and Bf.hi == None) and (Bg.lo == None and Bg.hi == None)   
+        if (Bf.lo == None and Bf.hi == None) and (Bg.lo == None and Bg.hi == None):  
             str = Bf + str(op) + Bg
         return (str)
     # Algoritmo exisits

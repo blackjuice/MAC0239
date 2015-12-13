1583c035f93e34eaa9f736d0e4f49f1fff1d48fd
leodaneu <leonardodaneu93@gmail.com>
2015-12-13 18:11:44 -0200

    Create functions.py

---
+++ EP2/functions.py
@@ -0,0 +1,20 @@
+# Algoritmo apply
+    # Bf = Bx', Bg = B->
+    def Apply(op, Bf, Bg):
+        str = Bf + str(op) + Bg
+        if (Bf.lo ~= None and Bf.hi ~= None) and (Bg.lo ~= None and Bg.hi ~= None)
+            Apply(op, Bf.hi, Bg.hi)
+            Apply(op, Bf.lo, Bg.lo)
+        if (Bf.lo ~= None and Bf.hi ~= None) and (Bg.lo == None and Bg.hi == None)
+            Apply(op, Bf.hi, Bg)
+            Apply(op, Bf.lo, Bg)
+        if (Bf.lo == None and Bf.hi == None) and (Bg.lo ~= None and Bg.hi ~= None)
+            Apply(op, Bf, Bg.hi)
+            Apply(op, Bf, Bg.lo)
+        if (Bf.lo == None and Bf.hi == None) and (Bg.lo == None and Bg.hi == None)   
+            str = Bf + str(op) + Bg
+        return (str)
+    # Algoritmo exisits
+    def Exists(op, Bf, x):
+        return Apply(op, Bf.restric(x, 0), Bf.restrict(x, 1))
+    

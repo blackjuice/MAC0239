cc940efd89fa68e1723177347f03f8983403c5f0
Lucas Hong <lucas.sj.hong@gmail.com>
2015-12-14 14:29:57 -0500

    b_prime funcionando....acho

--- EP2/ep2_8516816_8124329.py
+++ EP2/ep2_8516816_8124329.py
@@ -21,15 +21,29 @@ def separaParenteses_em_grupos (str_tmp):
 def array_all_int (str):
     str_tmp = re.sub(r'\D', " ", str)
     high_Xi = [int(s) for s in str_tmp.split() if s.isdigit()]
-    #print(max(high_Xi)); return int( max(high_Xi) )
     return (high_Xi)
+
+# recebe um array de inteiro
+# e devolve o array com sinais
+def array_muda_sinal (array_tmp, str):
+    i = 0; guarda_sinal = False
+    for c in str:
+        if (c == "-" or c == "~"):
+            guarda_sinal = True
+
+        if c.isdigit():
+            if guarda_sinal == True:
+                array_tmp[i] = -array_tmp[i]
+            i = i + 1
+            guarda_sinal = False
+    return array_tmp
 
 # Standard valor no dicionario
 def default_ddt_value (rotulos, numeroEstados):
     # criando uma conjuncao com todas as proposicoes envolvidas
-    high_Xi = int ( max(array_all_int (rotulos)) ) # maior proposicao
-    str_all_value = "~X[0]"
-    for i in range( 1, high_Xi ):
+    high_Xi = int ( max(array_all_int (rotulos)) ) + 1 # maior proposicao
+    str_all_value = "~X[1]"
+    for i in range( 2, high_Xi ):
         str_all_value = str_all_value + " & " + "~" + "X[" + str(i) + "]"
 
     # Todos os valores do dicionario contem conjuncao do tipo PI( ~x_i )
@@ -43,8 +57,7 @@ def default_ddt_value (rotulos, numeroEstados):
 def update_ddt_value (rotulos, numeroEstados, ddt):
     # guarda todos os estados em uma lista
     arrayStr_listaEstados = separaParenteses_em_grupos (rotulos)
-
-    for i in range( numeroEstados ):
+    for i in range(numeroEstados):
         array_tmp = ""
         # Lemos um rotulo por vez
         array_tmp = array_all_int ( arrayStr_listaEstados[i] )
@@ -54,12 +67,13 @@ def update_ddt_value (rotulos, numeroEstados, ddt):
             # verificamos cada proposicao
             for j in range( len(array_tmp) ):
                 str_tmp = "" # limpamos str_tmp
-                str_tmp_new = "X[" + str( array_tmp[j] - 1 ) + "]"
+                str_tmp_new = "X[" + str( array_tmp[j] ) + "]"
                 str_tmp = "~" + str_tmp_new
 
                 # atualizamos o value do key[i]
                 str_tmp = ddt[i].replace(str_tmp, str_tmp_new)
                 ddt[i] = str_tmp
+
     return (ddt)
 
 # Converte X para Y (ou X')
@@ -86,6 +100,52 @@ def write_B_arrow (kripke, ddt):
     b_arrow = b_arrow[3:] # apagando 3 primeiros char
     return (b_arrow)
 
+# construindo agora o Bx', que eh mais complicado.
+def write_B_xPrime (modeloPhi):
+
+    if (modeloPhi == "0" or modeloPhi == "1"):
+        return modeloPhi
+
+    i = 0; b_prime = ""; guarda_operador = ""; guarda_proposicao = False;
+    for c in modeloPhi:
+        if (c == "+" or c == "*"):
+            guarda_operador = c
+
+    array_tmp = array_all_int (modeloPhi)
+    modeloPhi_array = array_muda_sinal (array_tmp, modeloPhi)
+
+    # percorre dicionario e procura todas as proposicoes
+    # envolvidas no modelo
+    for i in range(numeroEstados):
+        array_tmp = array_muda_sinal ( (array_all_int (ddt[i])) , ddt[i])
+
+        if guarda_operador != "":
+            if (guarda_operador == "*"):
+                for j in range( len(modeloPhi_array) ):
+                    hold = False
+                    if modeloPhi_array[j] in array_tmp:
+                        hold = True
+                    else: break;
+            if (guarda_operador == "+"):
+                for j in range( len(modeloPhi_array) ):
+                    hold = False
+                    if modeloPhi_array[j] in array_tmp:
+                        hold = True; break;
+
+        else:
+            hold = False
+            if modeloPhi_array[0] in array_tmp:
+                hold = True
+
+        if hold == True:
+            b_prime = b_prime + " | " + ddt[i]
+
+    b_prime = b_prime[3:] # apagando 3 primeiros char
+    b_prime = convert_X_to_Xprime (b_prime)
+    return b_prime
+
+
+
 #------------main--------------
 numeroEstados = int(input())
 kripke = input()
@@ -93,12 +153,12 @@ rotulos = input()
 #formulaCTL = str( CTLtree( input() ) )
 formulaCTL = CTLtree( input() )
 interest = input()
-X = bddvars("x", numeroEstados)
-Y = bddvars("y", numeroEstados)
+X = bddvars("x", numeroEstados + 1)
+Y = bddvars("y", numeroEstados + 1)
 
 # criando dicionario
 ddt = default_ddt_value (rotulos, numeroEstados)
-
+#print (ddt); print ()
 # atualizamos dicionario
 ddt = update_ddt_value (rotulos, numeroEstados, ddt)
 #print (ddt)
@@ -106,24 +166,42 @@ ddt = update_ddt_value (rotulos, numeroEstados, ddt)
 # construimos B->
 b_arrow = write_B_arrow (kripke, ddt)
 
-# construindo agora o Bx', que eh mais complicado.
-def write_B_xPrime (modeloPhi):
-
-
-    return 0
+# construimos Bx
+modeloPhi = "+(x1)(x2)"
+#modeloPhi = "x3"
+#modeloPhi = "1"
+b_prime =  write_B_xPrime (modeloPhi)
+print (b_prime)
 
 '''
-print (b_arrow)
 
-#b_arrow = expr(b_arrow)
-#b_arrow = expr2bdd(b_arrow)
-#print ( b_arrow.satisfy_one() )
-#print ( list( b_arrow.satisfy_all() ) )
+b_result = "( " + b_arrow + ") & (" + b_prime + ")"
+
+#print (b_result)
+b_result = expr2bdd( expr(b_result) )
+#print ( list( b_result.satisfy_all() ) )
 
 
-testing
-#CTLtree.parse(str)
-str_tmp = "AU(AU(EX 1)(x1))(EU(x2)(0))"
-print (CTLtree(str_tmp))
-print (str(CTLtree.parse(str_tmp)) )
+x, y, z = map(bddvar, 'xyz')
+f = ~x & ~y & ~z & 1
+print (list(f.satisfy_all()))
+print (f)
+print (truthtable2expr(f))
+print (f.is_one())
+print (f.is_zero())
+
+
+A = exprvars('a', 3)
+#print (f.satisfy_count())
+f = truthtable(A, "00000000")
+if f.satisfy_one() == None:
+    print ("okay....")
+
+
+NO FINAL
+if f.satisfy_one() == None:
+    print ("UNSAT")
+else:
+    print ("SAT")
+    print ("lista de todos os estados que SAT")
 '''

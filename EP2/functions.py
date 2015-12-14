1dc79bcfc94b7f7e921cd56baa1e468ff9bcf9a3
leodaneu <leonardodaneu93@gmail.com>
2015-12-14 21:00:47 -0200

    Update functions.py

--- EP2/functions.py
+++ EP2/functions.py
@@ -1,8 +1,9 @@
     
-    # OBS: op = *
+# Algoritmo exists
 def Exists(Bf, x):
     return (Bf.restric({x: 0}) & Bf.restrict({x: 1}))
 
+# Algoritmo SAT
 def SAT(phi, S):    
     if (phi.kind == "1"):
         return S
@@ -14,12 +15,12 @@ def SAT(phi, S):
         return  phi.kind & S.restrict({phi.kind:1})
  
     if (phi.kind == "-"):
-        SAT(phi.child[0], S))
+        return (phi.child[0] & S.restrict({phi.child[0]: 0}))
 
-    if (phi.kind == "+" and phi.child[0] ~= None and phi.child[1] ~= None):
+    if (phi.kind == "+" and phi.child[0] != None and phi.child[1] ~= None):
         return(SAT(phi.child[0], S) | (SAT(phi.child[1], S)) 
 
-    if (phi.kind == "*" and phi.child[0] ~= None and phi.child[1] ~= None):
+    if (phi.kind == "*" and phi.child[0] != None and phi.child[1] ~= None):
         return(SAT(phi.child[0], S) & (SAT(phi.child[1], S)) 
 
     if (phi.kind == "AX"):
@@ -48,28 +49,22 @@ def SAT(phi, S):
         return(SAT(CTLfree.parse("- EF -" + str(phi.child[0])), S))
 
 
-
 def SAT_AF(phi, S):
     X = S
     Y = SAT(phi, S)
-    while (X ~= Y):
+    while (X != Y):
         X = Y
         Y = Y | Pre_forte(Y)
     return (Y)
-
-
-
 
 def SAT_EU(phi, psi, S):
     W = SAT(phi, S)
     Y = SAT(psi, S)
     X = S
-    while (X ~= Y):
+    while (X != Y):
         X = Y  
         Y = Y | (W & Pre_fraca(Y))
     return (Y)    
-    
-        
 
 def SAT_EX(phi, S):
     X = SAT(phi, S)
